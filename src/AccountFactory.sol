fbaf237f056fddc2daf761fe98f8f658af27b915
Ezeozue Chiagoziem <ezeozuechiagoziem@gmail.com>
2025-11-07 03:57:33 +0100

    Feat: Added Smart Account, Paymaster and account factory

---
+++ src/AccountFactory.sol
@@ -0,0 +1,59 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {SmartUserAccount} from "./SmartUserAccount.sol";
+import { IEntryPoint } from"lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
+import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import { Create2 } from "@openzeppelin/contracts/utils/Create2.sol";
+
+contract AccountFactory {
+    SmartUserAccount public immutable ACCOUNT_IMPLEMENTATION;
+
+    address[] public smartAccounts;
+
+    constructor(IEntryPoint _entryPoint) {
+        ACCOUNT_IMPLEMENTATION = new SmartUserAccount(_entryPoint);
+    }
+
+    /**
+     * @notice Create an account, and return its address
+     * @dev returns the address even if the account is already deployed.
+     * Note that during UserOperation execution, this method is called only if the account is not deployed.
+     * This method returns an existing account address so that entryPoint.getSenderAddress() would work even after account creation
+    */
+    function createAccount(address owner, uint256 salt) public returns (SmartUserAccount ret) {
+        address addr = getAddress(owner, salt);
+        uint256 codeSize = addr.code.length;
+        if (codeSize > 0) {
+            return SmartUserAccount(payable(addr));
+        }
+        ret = SmartUserAccount(
+            payable(
+                new ERC1967Proxy{salt: bytes32(salt)}(
+                    address(ACCOUNT_IMPLEMENTATION),
+                    abi.encodeCall(SmartUserAccount.initialize, (owner))
+                )
+            )
+        );
+
+        smartAccounts.push(address(ret));
+    }
+
+    /**
+     * @notice Calculate the counterfactual address of this account as it would be returned by createAccount()
+     */
+    function getAddress(address owner, uint256 salt) public view returns (address) {
+        return Create2.computeAddress(
+            bytes32(salt),
+            keccak256(
+                abi.encodePacked(
+                    type(ERC1967Proxy).creationCode,
+                    abi.encode(
+                        address(ACCOUNT_IMPLEMENTATION),
+                        abi.encodeCall(SmartUserAccount.initialize, (owner))    
+                    )
+                )
+            )
+        );
+    }
+}
